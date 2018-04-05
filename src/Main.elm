75781de9cd4f689bf0fb2800bf8c80511ca33bdd
Emmanuel Sales <absolutezero2a03@gmail.com>
2018-04-05 07:26:47 -0700

    Experimenting with dice

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,31 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program, button)
+import Html exposing (Html, div, h1, text, program, button)
 import Html.Events exposing (onClick)
+import Random
 
 type alias Model =
-    Int
+    { dieFace : Int
+
+    }
 
 init: (Model, Cmd Msg)
 init =
-    ( 1, Cmd.none )
+    ( Model 1, Cmd.none )
 
 -- MESSAGES
 
 type Msg
-    = Increment | Decrement
+    = Roll
+    | NewFace Int
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ h1 [] [ text (toString model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
         ]
 
 -- UPDATE
@@ -30,11 +33,12 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+        Roll ->
+            ( model, Random.generate NewFace (Random.int 1 6))
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        NewFace i ->
+            ( Model i, Cmd.none )
+
 
 -- SUBSCRIPTIONS
 
